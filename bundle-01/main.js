e6c956a6ce94e8340d010b52777f9ad7a567fe5e
Alessandro-Moroni <alemoro98@gmail.com>
2024-03-08 15:38:28 +0100

    [did]bundle-1

--- bundle-01/main.js
+++ bundle-01/main.js
@@ -11,37 +11,39 @@
 
 
 // ESERCIZIO 1
-for (let i = 0; i > 5; i++) {
+for (let i = 0; i < 5; i++) {
     console.log(i);
 }
-
+// < di 5 perche l'indice parte da 0 e non puo partire da 5
 
 // ESERCIZIO 2
 function addIfEven(num) {
-    if (num % 2 = 0) {
+    if (num % 2 === 0) {
         return num + 5;
     }
     return num;
 }
-
+// bisogna mettere 3 = e non 1 perche non viene confrontato
 
 // ESERCIZIO 3
 function loopToFive() {
-    for (let i = 0, i < 5, i++) {
+    for (let i = 0; i < 5; i++) {
         console.log(i);
     }
 }
-
+// vanno messi ; e non le ,
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
+    for (let i = 0; i < numbers.length ; i++) {
+        if (numbers[i] % 2 === 0) {
+            evenNumbers.push(numbers[i]);
         }
-        return evenNumbers;
     }
+    return evenNumbers;
 }
 displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+// tolto -1 ; aggiunto [i] === togliere ; aggiungere numbers[i] mettere il return fuori dal ciclo for
+console.log(displayEvenNumbers());
